659d72cfe1e128d9d7c0869451f3e64cb7368ed4
bielleb <bgbal@optonline.net>
2021-09-24 13:16:23 -0400

    feat(ui): added input validation with useForm for login

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,28 +1,63 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import {
   FormLabel,
   FormControl,
   Input,
   Button,
   Heading,
+  FormErrorMessage,
 } from '@chakra-ui/react';
 
 import styles from '@/styles/Admin.module.css';
 
 export default function Admin() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  function onSubmit() {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, 3000);
+    });
+  }
+
   return (
     <div className={styles.container}>
       <Heading>Login</Heading>
-      <form>
-        <FormControl>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormControl isInvalid={errors.username}>
           <FormLabel htmlFor="username">Username</FormLabel>
-          <Input id="username" placeholder="username" />
+          <Input
+            id="username"
+            placeholder="username"
+            {...register('username', {
+              required: 'This is required',
+            })}
+          />
+          <FormErrorMessage>
+            {errors.username && errors.username.message}
+          </FormErrorMessage>
         </FormControl>
-        <FormControl>
+        <FormControl isInvalid={errors.password}>
           <FormLabel htmlFor="password">Password</FormLabel>
-          <Input id="password" placeholder="password" />
+          <Input
+            id="password"
+            placeholder="password"
+            {...register('password', {
+              required: 'This is required',
+              minLength: { value: 8, message: 'Minimum length should be 8' },
+            })}
+          />
+          <FormErrorMessage>
+            {errors.password && errors.password.message}
+          </FormErrorMessage>
         </FormControl>
-        <Button mt={4} type="submit">
+        <Button mt={4} isLoading={isSubmitting} type="submit">
           Submit
         </Button>
       </form>
