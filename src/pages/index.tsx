09ad61bbc58f3b18117b30263600c08be619d5c0
MeghanKulkarni <kulkarni.meg@gmail.com>
2021-09-23 23:35:42 -0400

    feat(context): initialized landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,43 @@
-import Head from 'next/head';
+// import Head from 'next/head';
 import Image from 'next/image';
 
+import { Box } from '@chakra-ui/react';
 import styles from '@/styles/Home.module.css';
+import classes from '@/styles/index.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
+      {/* <Head>
         <title>Pencils for Success</title>
         <meta name="description" content="Pencils for Success" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head> */}
+
+      <div className={classes.title}>
+        <h1>Select Your Chapter</h1>
+      </div>
+
+      <div className={classes.container}>
+        <Box className={classes.cell}>
+          <div className={classes.circlePic}>
+            <Image src="/Ellipse1.png" width={120} height={120} />
+          </div>
+          <p className={classes.cellTitle}>Georgia</p>
+        </Box>
+        <Box className={classes.cell}>
+          <div className={classes.circlePic}>
+            <Image src="/Ellipse1.png" width={120} height={120} />
+          </div>
+          <p className={classes.cellTitle}>Kansas</p>
+        </Box>
+        <Box className={classes.cell}>
+          <div className={classes.circlePic}>
+            <Image src="/Ellipse1.png" width={120} height={120} />
+          </div>
+          <p className={classes.cellTitle}>Texas</p>
+        </Box>
+      </div>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Pencils for Success</h1>
