5d9646596616ee2ce01cf3fad0a8ecb247d2ef24
bielleb <bgbal@optonline.net>
2021-09-24 12:13:03 -0400

    build: importing React hooks useform

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,19 +1,31 @@
-import { FormLabel, FormControl, Input, Button } from '@chakra-ui/react';
+import React from 'react';
+import {
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+  Heading,
+} from '@chakra-ui/react';
+
+import styles from '@/styles/Admin.module.css';
 
 export default function Admin() {
   return (
-    <form>
-      <FormControl>
-        <FormLabel htmlFor="name">Username</FormLabel>
-        <Input id="username" placeholder="username" />
-      </FormControl>
-      <FormControl>
-        <FormLabel htmlFor="name">Password</FormLabel>
-        <Input id="password" placeholder="password" />
-      </FormControl>
-      <Button mt={4} colorScheme="teal" type="submit">
-        Submit
-      </Button>
-    </form>
+    <div className={styles.container}>
+      <Heading>Login</Heading>
+      <form>
+        <FormControl>
+          <FormLabel htmlFor="username">Username</FormLabel>
+          <Input id="username" placeholder="username" />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor="password">Password</FormLabel>
+          <Input id="password" placeholder="password" />
+        </FormControl>
+        <Button mt={4} type="submit">
+          Submit
+        </Button>
+      </form>
+    </div>
   );
 }
