1f0858fed8cd11f0b238eb205c80e7191be61c02
Ishan Baniya <ibaniya98@gmail.com>
2021-09-30 16:32:46 -0400

    feat(toosl): generate hash for each user with unique password

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,29 +1,37 @@
 import { PrismaClient } from '@prisma/client';
+import { getPasswordHash } from '../src/utils/password';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  const password = 'bitsofgood';
-  // TODO - Use bcrypt to generate a hash here
-  const passwordHash = password;
+// List of passwords for initial User accounts
+const PASSWORDS = {
+  basicUser: 'bitsofgood',
+  adminUser: 'bog_admin',
+  chapterUser: 'bog_chapter',
+  recipientUser: 'bog_recipient',
+};
 
+// Create basic models and relations
+async function main() {
   // Basic User
+  const basicUserPassword = await getPasswordHash(PASSWORDS.basicUser);
   await prisma.user.upsert({
     where: { username: 'panda' },
     update: {},
     create: {
       username: 'panda',
-      hash: passwordHash,
+      hash: basicUserPassword,
     },
   });
 
   // Admin User only
+  const adminUserPassword = await getPasswordHash(PASSWORDS.adminUser);
   await prisma.user.upsert({
     where: { username: 'admin' },
     update: {},
     create: {
       username: 'admin',
-      hash: passwordHash,
+      hash: adminUserPassword,
       admin: {
         create: {},
       },
@@ -41,6 +49,7 @@ async function main() {
   });
 
   // Chapter with a chapter user
+  const chapterUserPassword = await getPasswordHash(PASSWORDS.chapterUser);
   await prisma.chapter.upsert({
     where: { email: 'georgia@pfs.org' },
     update: {},
@@ -55,7 +64,7 @@ async function main() {
           user: {
             create: {
               username: 'georgia_chapter',
-              hash: passwordHash,
+              hash: chapterUserPassword,
             },
           },
         },
@@ -76,12 +85,13 @@ async function main() {
   });
 
   // Recipient User with Recipient
+  const recipientUserPassword = await getPasswordHash(PASSWORDS.recipientUser);
   await prisma.user.upsert({
     where: { username: 'recipient_user' },
     update: {},
     create: {
       username: 'recipient_user',
-      hash: passwordHash,
+      hash: recipientUserPassword,
       recipient: {
         create: {
           recipient: {
