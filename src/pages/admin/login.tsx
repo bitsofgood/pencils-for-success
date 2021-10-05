cc83dfd2725fc607d8dfcebfdd6f34530e633a4b
Arthelon <hsing.daniel@gmail.com>
2021-10-04 22:31:10 -0400

    refactor: use arrow functions instead of anon for submit handler
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import Login from '@/components/Login';
 
 export default function Admin() {
-  function onSubmit() {
-    return new Promise<void>((resolve) => {
+  const onSubmit = () =>
+    new Promise<void>((resolve) => {
       setTimeout(() => {
         resolve();
       }, 3000);
     });
-  }
 
-  // eslint-disable-next-line react/jsx-no-bind
   return <Login onSubmit={onSubmit} />;
 }
