d519f35fb540907d67b2f7bc096a33e196cece9a
bielleb <bgbal@optonline.net>
2021-10-12 23:44:48 -0400

    refactor(ui): change page component at recipient/login from ChapterLogin to RecipientLogin error fix

--- src/pages/recipient/login.tsx
+++ src/pages/recipient/login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Login from '@/components/Login';
 
-export default function ChapterLogin() {
+export default function RecipientLogin() {
   const onSubmit = () =>
     new Promise<void>((resolve) => {
       setTimeout(() => {
