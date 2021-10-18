36cfd8103cee7e2f6d67a5fb146e9119e94dff3c
Ishan Baniya <ibaniya98@gmail.com>
2021-10-18 13:23:00 -0400

    feat(backend): add endpoint to update chapter only

--- src/pages/api/chapters/[chapterId].ts
+++ src/pages/api/chapters/[chapterId].ts
@@ -1,8 +1,16 @@
 import type { NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client';
 import { ErrorResponse, serverErrorHandler } from '@/utils/error';
 import { NextIronRequest } from '@/utils/session';
-import { withAdminRequestSession } from '@/utils/middlewares/auth';
+import {
+  withAdminRequestSession,
+  withAuthedRequestSession,
+} from '@/utils/middlewares/auth';
+import { SessionAdminUser } from '../admin/login';
+
+interface ChapterUpdateBody {
+  updatedChapter: Prisma.ChapterCreateInput;
+}
 
 async function handler(
   req: NextIronRequest,
@@ -10,8 +18,75 @@ async function handler(
 ) {
   const { chapterId } = req.query;
 
+  const user = req.session.get('user') as SessionAdminUser;
+
+  // Verify the provided id is a valid chapter id
+  if (Number.isNaN(chapterId)) {
+    return res.status(400).json({
+      message: 'Please provide a valid chapter id',
+      error: true,
+    });
+  }
+
   switch (req.method) {
+    case 'PUT':
+      // TODO - determine if the user is Chapter User
+      if (!user.admin || false) {
+        return res.status(400).json({
+          message: 'Please login as an authorized user to access the resource',
+          error: true,
+        });
+      }
+      try {
+        const prisma = new PrismaClient();
+        const chapterToUpdate = await prisma.chapter.findUnique({
+          where: {
+            id: Number(chapterId),
+          },
+        });
+
+        if (!chapterToUpdate) {
+          return res.status(400).json({
+            message: 'No valid chapter found for the given chapter id',
+            error: true,
+          });
+        }
+
+        const { updatedChapter } = req.body as ChapterUpdateBody;
+        if (!updatedChapter) {
+          return res.status(400).json({
+            message: 'Please provide valid request body with chapter details',
+            error: true,
+          });
+        }
+
+        const data = {
+          ...chapterToUpdate,
+          ...updatedChapter,
+        };
+
+        await prisma.chapter.update({
+          where: {
+            id: Number(chapterId),
+          },
+          data,
+        });
+
+        return res.status(200).json({
+          message: 'Successfully updated the chapter',
+          error: false,
+        });
+      } catch (e) {
+        // console.error(e);
+        return serverErrorHandler(e, res);
+      }
     case 'DELETE':
+      if (!user.admin) {
+        return res.status(400).json({
+          message: 'Please login as an admin to access the resource',
+          error: true,
+        });
+      }
       try {
         const prisma = new PrismaClient();
 
@@ -70,11 +145,11 @@ async function handler(
       }
 
     default:
-      res.setHeader('Allow', ['DELETE']);
+      res.setHeader('Allow', ['PUT', 'DELETE']);
       return res
         .status(405)
         .json({ error: true, message: `Method ${req.method} Not Allowed` });
   }
 }
 
-export default withAdminRequestSession(handler);
+export default withAuthedRequestSession(handler);
--- src/utils/middlewares/auth.ts
+++ src/utils/middlewares/auth.ts
@@ -8,6 +8,23 @@ import {
   withSession,
 } from '../session';
 
+// Helper function for protecting API routes, returns 401 response if not authed
+export function withAuthedRequestSession(
+  handler: Handler<NextIronRequest, NextApiResponse>,
+) {
+  return withSession((req: NextIronRequest, res: NextApiResponse) => {
+    const user = req.session.get('user');
+    if (user) {
+      handler(req, res);
+    } else {
+      res.status(401).json({
+        isLoggedIn: false,
+        message: 'Not Authenticated',
+      });
+    }
+  });
+}
+
 /**
  * Helper function for protecting Admin only API routes.
  * Returns 401 response if unauthenticated or unauthorized.
@@ -23,6 +40,31 @@ export function withAdminRequestSession(
       res.status(401).json({
         isLoggedIn: false,
         message: 'Please login as an admin to access the resource',
+      });
+    }
+  });
+}
+
+/**
+ * Helper function for protecting Admin or Chapter User only API routes
+ * Returns 401 response is unauthenticated or unauthorized
+ */
+export function withAdminOrChapterUserRequestSession(
+  handler: Handler<NextIronRequest, NextApiResponse>,
+) {
+  return withSession(async (req: NextIronRequest, res: NextApiResponse) => {
+    const user = req.session.get('user') as SessionAdminUser;
+
+    // TODO - Simply the boolean expression once endpoint to login as chapter user is implemented
+    const isAdmin = user && user.isLoggedIn && user.admin;
+    const isChapterUser = false;
+    if (isAdmin || isChapterUser) {
+      await handler(req, res);
+    } else {
+      res.status(401).json({
+        isLoggedIn: false,
+        message:
+          'Please login as authorized chapter user or admin to access the resource',
       });
     }
   });
