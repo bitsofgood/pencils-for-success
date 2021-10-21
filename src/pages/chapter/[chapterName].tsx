acf5c1dc2edc2c97b21e8466f1b87ff2f01fa870
Ethan Xie <ethankeystone@live.com>
2021-10-21 02:22:14 -0400

    fix: eslint-issue

--- src/pages/chapter/[chapterName].tsx
+++ src/pages/chapter/[chapterName].tsx
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 import { ErrorResponse, serverErrorHandler } from '@/utils/error';
 import { NextIronRequest } from '@/utils/session';
 import { withAdminRequestSession } from '@/utils/middlewares/auth';
-import { SessionAdminUser } from '../admin/login';
+
 
 export type ChapterInfo = {
   email: string;
