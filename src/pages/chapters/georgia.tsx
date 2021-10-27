710209c51639243041a8788aaa77b9c42fccc80f
MeghanKulkarni <kulkarni.meg@gmail.com>
2021-10-26 23:21:14 -0400

    feat(ui): added grids to portal pages

---
+++ src/pages/chapters/georgia.tsx
@@ -0,0 +1,66 @@
+import Image from 'next/image';
+
+import { Box } from '@chakra-ui/react';
+import styles from '@/styles/Home.module.css';
+import classes from '@/pages/chapters/portal.module.css';
+
+// import NavBar from '@/components/navbar';
+// import DonorNavBar from '@/components/donorNavbar';
+
+export default function ChapterPortal() {
+  return (
+    <div className={styles.container}>
+      <div className={classes.title}>
+        <h1>Georgia Chapter Recipients</h1>
+      </div>
+
+      <div className={classes.container}>
+        <Box className={classes.cell}>
+          <p className={classes.cellTitle}>Georgia School #1</p>
+          <p className={classes.cellText}> 5 Pending Supply Requests</p>
+        </Box>
+        <Box className={classes.cell}>
+          <p className={classes.cellTitle}>Georgia School #2</p>
+          <p className={classes.cellText}> 5 Pending Supply Requests</p>
+        </Box>
+        <Box className={classes.cell}>
+          <p className={classes.cellTitle}>Georgia School #3</p>
+          <p className={classes.cellText}> 5 Pending Supply Requests</p>
+        </Box>
+        <Box className={classes.cell}>
+          <p className={classes.cellTitle}>Georgia School #4</p>
+          <p className={classes.cellText}> 5 Pending Supply Requests</p>
+        </Box>
+        <Box className={classes.cell}>
+          <p className={classes.cellTitle}>Georgia School #5</p>
+          <p className={classes.cellText}> 5 Pending Supply Requests</p>
+        </Box>
+        <Box className={classes.cell}>
+          <p className={classes.cellTitle}>Georgia School #6</p>
+          <p className={classes.cellText}> 5 Pending Supply Requests</p>
+        </Box>
+        <Box className={classes.cell}>
+          <p className={classes.cellTitle}>Georgia School #7</p>
+          <p className={classes.cellText}> 5 Pending Supply Requests</p>
+        </Box>
+        <Box className={classes.cell}>
+          <p className={classes.cellTitle}>Georgia School #8</p>
+          <p className={classes.cellText}> 5 Pending Supply Requests</p>
+        </Box>
+      </div>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=typescript-nextjs-starter"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{` `}
+          <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  );
+}
