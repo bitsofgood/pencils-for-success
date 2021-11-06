7e21a8f1ecf2a69b304a5eb78bdecf9335c73ab3
Arthelon <hsing.daniel@gmail.com>
2021-11-06 12:00:25 -0400

    chore(ui): remove vercel boilerplate, install react-map-gl
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,17 @@
 import Head from 'next/head';
 import Image from 'next/image';
 
-import styles from '@/styles/Home.module.css';
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Pencils for Success</title>
         <meta name="description" content="Pencils for Success" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Pencils for Success</h1>
+      <main>
+        <h1>Pencils for Success</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{` `}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
