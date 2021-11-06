13730254d842c6bee2bcefa7cd4986cbb5ba3d44
bielleb <bgbal@optonline.net>
2021-11-06 12:42:51 -0400

    feat(ui):  implemented login endpoint for admin page

--- src/pages/recipient/login.tsx
+++ src/pages/recipient/login.tsx
@@ -2,12 +2,5 @@ import React from 'react';
 import Login from '@/components/Login';
 
 export default function RecipientLogin() {
-  const onSubmit = () =>
-    new Promise<void>((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, 3000);
-    });
-
-  return <Login onSubmit={onSubmit} title="Donation Recipient" />;
+  return <Login apiURL="" replaceURL="" title="Donation Recipient" />;
 }
