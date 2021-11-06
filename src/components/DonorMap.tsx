9052f39684ab09a9f7315f781c9bdb2a851fdcf9
Arthelon <hsing.daniel@gmail.com>
2021-11-06 12:49:24 -0400

    feat(map): add geolocate control to display user location
    Signed-off-by: Arthelon <hsing.daniel@gmail.com>

--- src/components/DonorMap.tsx
+++ src/components/DonorMap.tsx
@@ -1,14 +1,39 @@
-import MapGL from 'react-map-gl';
+import { useState } from 'react';
+import MapGL, { GeolocateControl } from 'react-map-gl';
 import { Box } from '@chakra-ui/react';
 
+const geolocateStyle = {
+  top: 0,
+  left: 0,
+  margin: 10,
+  display: 'none', // hide control
+};
+const initialViewportState = {
+  latitude: 33.749,
+  longitude: -84.38633,
+  zoom: 12,
+};
+
 function DonorMap() {
+  const [viewport, setViewport] = useState(initialViewportState);
+
   return (
     <Box pos="absolute" top="0" right="0" bottom="0" left="0">
       <MapGL
+        {...viewport}
         width="100%"
         height="100%"
+        onViewportChange={setViewport}
         mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
-      />
+      >
+        <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={{
+            enableHighAccuracy: true,
+          }}
+          auto
+        />
+      </MapGL>
     </Box>
   );
 }
