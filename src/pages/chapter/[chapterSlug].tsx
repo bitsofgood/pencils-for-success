fedeeb82357a177c8ec9bb6f7bb90e5832f510e5
Ishan Baniya <ibaniya98@gmail.com>
2021-11-06 13:33:13 -0400

    feat(frontend): reset donor chapter page

--- src/pages/chapter/[chapterSlug].tsx
+++ src/pages/chapter/[chapterSlug].tsx
@@ -1,21 +1,9 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Heading,
-  SimpleGrid,
-  Spacer,
-} from '@chakra-ui/react';
-import { PrismaClient, Chapter, Recipient } from '@prisma/client';
+import { Box, Flex, Heading, Spacer } from '@chakra-ui/react';
+import { PrismaClient, Chapter } from '@prisma/client';
 import React from 'react';
-import RecipientCard from '@/components/RecipientCard';
-
-type ChapterWithRecipients = Chapter & {
-  recipients: Recipient[];
-};
 
 interface ChapterMapPageProps {
-  chapter: ChapterWithRecipients;
+  chapter: Chapter;
 }
 
 interface IStaticPropsContextParams {
@@ -24,42 +12,13 @@ interface IStaticPropsContextParams {
   };
 }
 
-function AddNewRecipientButton() {
-  const onNewRecipientClick = () => {
-    alert('Open modal to create new recipient');
-  };
-
-  return (
-    <Button onClick={onNewRecipientClick} colorScheme="blue">
-      + Add New
-    </Button>
-  );
-}
-
-interface RecipientCardProps {
-  recipients: Recipient[];
-}
-
-function RecipientCardsGrid({ recipients }: RecipientCardProps) {
-  return (
-    <SimpleGrid columns={{ base: 1, md: 2, lg: 5 }} my="5" spacing="5">
-      {recipients.map((x) => (
-        <RecipientCard recipient={x} key={x.id} />
-      ))}
-    </SimpleGrid>
-  );
-}
-
-export default function ChapterMapPage({ chapter }: ChapterMapPageProps) {
+export default function ChapterDashboardPage({ chapter }: ChapterMapPageProps) {
   return (
     <Box p="10">
       <Flex>
-        <Heading>{chapter.chapterName} Chapter Recipients</Heading>
+        <Heading>{chapter.chapterName} Chapter </Heading>
         <Spacer />
-        <AddNewRecipientButton />
       </Flex>
-
-      <RecipientCardsGrid recipients={chapter.recipients || []} />
     </Box>
   );
 }
@@ -82,16 +41,12 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }: IStaticPropsContextParams) {
-  // TODO - Figure out how to allow case insensitive searches in Prisma
   const { chapterSlug } = params;
 
   const prisma = new PrismaClient();
   const chapter = await prisma.chapter.findUnique({
     where: {
       chapterSlug,
-    },
-    include: {
-      recipients: true,
     },
   });
 
