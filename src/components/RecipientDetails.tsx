f066e24f4da0428957b6c46ebfea90ca4fea2e09
Ishan Baniya <ibaniya98@gmail.com>
2021-11-10 13:24:37 -0500

    feat(frontend): add placeholder for supply requests

--- src/components/RecipientDetails.tsx
+++ src/components/RecipientDetails.tsx
@@ -1,22 +1,50 @@
 import { Box, Heading } from '@chakra-ui/react';
-import React from 'react';
+import { Recipient } from '@prisma/client';
+import React, { useContext } from 'react';
 import { IoIosSchool } from 'react-icons/io';
+import { RecipientsContext } from '@/providers/RecipientsProvider';
 
-function RecipientDetails() {
+interface RecipientDetailsProps {
+  recipientId: number;
+}
+
+function getActiveRecipient(recipientId: number, recipients: Recipient[]) {
+  const filteredRecipients = recipients.filter((x) => x.id === recipientId);
+  let activeRecipient = null;
+  if (filteredRecipients.length > 0) {
+    // eslint-disable-next-line prefer-destructuring
+    activeRecipient = filteredRecipients[0];
+  }
+  return activeRecipient;
+}
+
+function RecipientDetails({ recipientId }: RecipientDetailsProps) {
+  const { recipients } = useContext(RecipientsContext);
+  const activeRecipient = getActiveRecipient(recipientId, recipients);
+
+  if (!activeRecipient) {
+    return (
+      <Box
+        w="100%"
+        h="100%"
+        minH="500px"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDir="column"
+        fontSize="5xl"
+        color="gray.600"
+      >
+        <IoIosSchool />
+        <Heading>Select Recipient</Heading>
+      </Box>
+    );
+  }
+
   return (
-    <Box
-      w="100%"
-      h="100%"
-      minH="500px"
-      display="flex"
-      alignItems="center"
-      justifyContent="center"
-      flexDir="column"
-      fontSize="5xl"
-      color="gray.600"
-    >
-      <IoIosSchool />
-      <Heading>Select Recipient</Heading>
+    <Box bg="white" minH="500px" boxShadow="lg" borderRadius="lg" p="5">
+      <Heading>Supply Requests</Heading>
+      {activeRecipient.name}
     </Box>
   );
 }
