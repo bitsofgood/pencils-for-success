739a85c0ee4810571059a38eb44f73fa272460c2
Ishan Baniya <ibaniya98@gmail.com>
2021-11-10 21:06:17 -0500

    feat(frontend): show information for recipients on click

--- src/components/RecipientCard.tsx
+++ src/components/RecipientCard.tsx
@@ -1,5 +1,15 @@
-import { Heading, Flex } from '@chakra-ui/react';
+import {
+  Heading,
+  Flex,
+  Spacer,
+  Box,
+  Text,
+  UnorderedList,
+  ListItem,
+} from '@chakra-ui/react';
 import { Recipient } from '@prisma/client';
+import React, { useState } from 'react';
+import { BsThreeDots } from 'react-icons/bs';
 
 interface RecipientCardProps {
   recipient: Recipient;
@@ -7,23 +17,74 @@ interface RecipientCardProps {
   onClick: (id: number) => void;
 }
 
-function RecipientCard({ recipient, onClick }: RecipientCardProps) {
+interface RecipientDetailsProps {
+  recipient: Recipient;
+}
+
+function RecipientDetails({ recipient }: RecipientDetailsProps) {
   return (
-    <Flex
+    <Box>
+      <Box my="2">
+        <Heading size="sm">Contact Information</Heading>
+        <Text>{recipient.contactName}</Text>
+        <Text>{recipient.email}</Text>
+        <Text>{recipient.phoneNumber}</Text>
+      </Box>
+
+      <Box my="2">
+        <Heading size="sm">Address</Heading>
+        <Text>{recipient.primaryStreetAddress}</Text>
+        {recipient.secondaryStreetAddress && (
+          <Text>{recipient.secondaryStreetAddress}</Text>
+        )}
+        <Text>
+          {recipient.city}, {recipient.state} {recipient.postalCode}
+        </Text>
+      </Box>
+
+      <Box my="2">
+        <Heading size="sm">Username</Heading>
+        {/* <Text>{recipient?.user.username}</Text> */}
+      </Box>
+    </Box>
+  );
+}
+
+function SupplyRequestStatus() {
+  // TODO - Fetch this dynamically once implemented
+  const [pendingRequests, setPendingRequests] = useState(
+    Math.floor(Math.random() * 6),
+  );
+
+  const statusColor = pendingRequests > 0 ? '#CA9000' : '#858585';
+
+  return (
+    <UnorderedList color={statusColor}>
+      <ListItem>{pendingRequests} Pending</ListItem>
+    </UnorderedList>
+  );
+}
+
+function RecipientCard({ recipient, onClick, isActive }: RecipientCardProps) {
+  return (
+    <Box
       boxShadow="lg"
       borderRadius="lg"
-      borderWidth="1px"
       cursor="pointer"
-      direction="column"
-      alignItems="baseline"
       bg="white"
       padding="5"
       onClick={() => onClick(recipient.id)}
+      fontSize="sm"
     >
-      <Heading size="md" paddingBottom="5">
-        {recipient.name}
-      </Heading>
-    </Flex>
+      <Flex>
+        <Heading size="sm">{recipient.name}</Heading>
+        <Spacer />
+        <BsThreeDots />
+      </Flex>
+      <SupplyRequestStatus />
+
+      {isActive && <RecipientDetails recipient={recipient} />}
+    </Box>
   );
 }
 
