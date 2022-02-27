97223ae76545c55aea857dd33cf8aa009637847b
Varshita Patakottu <varshi@Varshitas-MBP.hsd1.ga.comcast.net>
2022-02-27 00:40:45 -0500

    feat: added dropdown

--- src/components/navbars/DonorNavbar.tsx
+++ src/components/navbars/DonorNavbar.tsx
@@ -1,12 +1,20 @@
-import { HStack, Button } from '@chakra-ui/react';
+import { HStack, Button, Select } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
 import NavbarContainer from './NavbarContainer';
+import { GetChapterResponse } from '@/pages/api/chapters';
 
 export default function DonorNavbar() {
   const router = useRouter();
+  const { data, error } = useSWR<GetChapterResponse>(`/api/chapters`);
   return (
     <NavbarContainer>
       <HStack spacing="15px">
+        <Select size="sm" variant="outline" w="40%">
+          {data?.chapters.map((chapter) => (
+            <option key={chapter.id}>{chapter.chapterName}</option>
+          ))}
+        </Select>
         <Button
           size="sm"
           variant="outline"
